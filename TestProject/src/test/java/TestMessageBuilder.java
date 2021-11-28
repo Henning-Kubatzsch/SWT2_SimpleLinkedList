a5f3aa1c782034a5213b6e8f08971e26e6831b42
Henning Kubatzsch <hilda@server.fritz.box>
2021-11-28 13:39:05 +0100

    Fixed JaCoCo declaration, added test examples

---
+++ TestProject/src/test/java/TestMessageBuilder.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMessageBuilder {
+
+    @Test
+    public void testNameMkyong() {
+
+        MessageBuilder obj = new MessageBuilder();
+        assertEquals("Hello mkyong", obj.getMessage("mkyong"));
+
+    }
+
+}
