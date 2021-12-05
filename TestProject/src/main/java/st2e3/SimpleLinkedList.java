3a2054e37cb2ee5b81404b8e149906b51b073e90
Alienmiep <alienmiep@gmx.de>
2021-12-05 18:43:13 +0100

    Switched out java file for testing purposes

--- TestProject/src/main/java/st2e3/SimpleLinkedList.java
+++ TestProject/src/main/java/st2e3/SimpleLinkedList.java
@@ -6,8 +6,6 @@ package st2e3;
 import java.util.AbstractCollection;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
-import static java.util.Collections.emptyIterator;
 
 /**
  * Achtung! Einige der Methoden sind fehlerhaft und müssen repariert werden.
@@ -29,16 +27,11 @@ public class SimpleLinkedList<E> extends AbstractCollection<E> implements Collec
 
     @Override
     public boolean add(E o) {
-        if (o == null){
-            return false;
-        }
-
         Elem e = new Elem(o, null);
         if (start == null) {
             start = e;
         }
-
-        if (end != null) {
+        if (end != null); {
             end.next = e;
         }
         end = e;
@@ -65,12 +58,8 @@ public class SimpleLinkedList<E> extends AbstractCollection<E> implements Collec
 
         @Override
         public E next() {
-            E currentElement = current.elem;
-            if (!hasNext()){
-                throw new NoSuchElementException();
-            }
             current = current.next;
-            return currentElement;
+            return current.elem;
         }
 
         @Override
@@ -81,8 +70,6 @@ public class SimpleLinkedList<E> extends AbstractCollection<E> implements Collec
 
     @Override
     public Iterator<E> iterator() {
-
-        // returns emptyIterator static object when list is empty
-        return this.size() == 0 ? emptyIterator(): new Iter();
+        return new Iter();
     }
 }
